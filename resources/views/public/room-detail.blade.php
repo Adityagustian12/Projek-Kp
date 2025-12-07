6c229da1a6a67bad250a8d70d025989a85841081
Adityagustian12 <123oikziro@gmail.com>
2025-12-08 01:05:19 +0700

    Revert to asset() for images - route not registering on server

--- resources/views/public/room-detail.blade.php
+++ resources/views/public/room-detail.blade.php
@@ -17,7 +17,11 @@
                             <div class="row g-2">
                                 @foreach($room->images as $image)
                                     <div class="col-6 col-md-4">
-                                        <img src="{{ route('storage.file', ['path' => $image]) }}" 
+                                        @php
+                                            // Use asset() directly - more reliable
+                                            $imageUrl = asset('storage/' . $image);
+                                        @endphp
+                                        <img src="{{ $imageUrl }}" 
                                              class="img-fluid rounded shadow-sm" 
                                              alt="Room {{ $room->room_number }}"
                                              style="height: 180px; width: 100%; object-fit: cover;"
